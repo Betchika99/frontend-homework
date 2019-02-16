3588f84e85c0edbcaf3be3996fa8a4cc3e376a6d
Betchika99 <dobrl2011@mail.ru>
2019-02-16 23:08:31 +0300

    Updated code style

--- source/letters.js
+++ source/letters.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const letters = ( string, flag ) => {
-	if ( ( typeof string !== 'string' ) || ( ( typeof flag !== 'boolean' ) && ( flag !== undefined ) ) ) {
+	if (typeof string !== 'string' || (typeof flag !== 'boolean' && typeof flag !== 'undefined')) {
+		console.log("BBBLAAAAA");
 		return string;
 	}
 
@@ -9,25 +10,27 @@ const letters = ( string, flag ) => {
 	let repeat = new Set();
 
 	// flag can be false or undefined - different logic in every way
-	if ( flag === false ) {
+	if (flag === false) {
 		string = string.split('').reverse().join('');	
 	}
 
 	let mode = flag === undefined ? 'all' : 'one';
 
-	for ( let ch of string ) {
-		let hasDouble = string.indexOf( ch ) !== string.lastIndexOf( ch ) ? true : false;
-		if ( !hasDouble ) {
-			result.add( ch );
-		} else if ( !repeat.has( ch ) ) {
-			repeat.add( ch );
-			mode === 'one' && result.add( ch );
+	for (let ch of string) {
+		let hasDouble = string.indexOf(ch) !== string.lastIndexOf(ch) ? true : false;
+		if (!hasDouble) {
+			result.add(ch);
+		} else if ( !repeat.has(ch) ) {
+			repeat.add(ch);
+			mode === 'one' && result.add(ch);
 		}
 	}
 
-	if ( flag === false ) {
+	if (flag === false) {
 		return Array.from(result).reverse().join('');	
 	}
 
 	return [...result].join('');
 }
+
+console.log(letters('111'));
